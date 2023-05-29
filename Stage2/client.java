9e9a78b701498f7d00bbd1f43168177c3f0e7c9d
Justin-zi <Justinling24@gmail.com>
2023-05-29 21:50:40 +1000

    test

--- Stage2/client.java
+++ Stage2/client.java
@@ -36,47 +36,44 @@ class client {
                 currMessages = receiveMessage(inputReader);
                 boolean first = currMessages.contains("JOBN") || currMessages.contains("JOBP");
                 boolean scheduled = false;
-                int count =0;
 
                 if (first) {
-                    String[] dataArray = currMessages.split(" ");
-                    int jobId = Integer.parseInt(dataArray[2]);
-                    int jobCores = Integer.parseInt(dataArray[4]);
-                    int jobMemory = Integer.parseInt(dataArray[5]);
-                    int jobDisk = Integer.parseInt(dataArray[6]);
+                    String[] serverInfo = currMessages.split(" ");
+                    int jobId = Integer.parseInt(serverInfo[2]);
+                    int jobCores = Integer.parseInt(serverInfo[4]);
+                    int jobMemory = Integer.parseInt(serverInfo[5]);
+                    int jobDisk = Integer.parseInt(serverInfo[6]);
 
                     sendMessage(outputStream, "GETS Capable " + jobCores + " " + jobMemory + " " + jobDisk);
                     messages = receiveMessage(inputReader);
 
-                    String[] dataArray2 = messages.split(" ");
-                    int numOfRecords = Integer.parseInt(dataArray2[1]);
+                    String[] serverInfoRec = messages.split(" ");
+                    int numOfRecords = Integer.parseInt(serverInfoRec[1]);
 
                     sendMessage(outputStream, "OK");
 
                     for (int i = 0; i < numOfRecords; i++) {
                         messages = receiveMessage(inputReader);
-                        String[] dataArray3 = messages.split(" ");
-                        count++;
+                        String[] serverInfoMain = messages.split(" ");
                         if (first) {
-                            firstServerName = dataArray3[0];
-                            serverID = Integer.parseInt(dataArray3[1]);
+                            firstServerName = serverInfoMain[0];
+                            serverID = Integer.parseInt(serverInfoMain[1]);
                             first = false;
                         }
 
-                        serverCores = Integer.parseInt(dataArray3[4]);
-                        serverMemory = Integer.parseInt(dataArray3[5]);
-                        serverDisk = Integer.parseInt(dataArray3[6]);
-                        noJobsWaiting = Integer.parseInt(dataArray3[7]);
+                        serverCores = Integer.parseInt(serverInfoMain[4]);
+                        serverMemory = Integer.parseInt(serverInfoMain[5]);
+                        serverDisk = Integer.parseInt(serverInfoMain[6]);
+                        noJobsWaiting = Integer.parseInt(serverInfoMain[7]);
                         
                         boolean resources = noJobsWaiting == 0 && jobCores <= serverCores && jobMemory <= serverMemory && jobDisk <= serverDisk && !scheduled;
 
                         if (resources) {
-                            serverType = dataArray3[0];
-                            scheduleID = Integer.parseInt(dataArray3[1]);
+                            serverType = serverInfoMain[0];
+                            scheduleID = Integer.parseInt(serverInfoMain[1]);
                             scheduled = true;
                         }
                     }
-                    count--;
                     sendMessage(outputStream, "OK");
                     messages = receiveMessage(inputReader);
 
